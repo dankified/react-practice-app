fd5d92e6c43b4c50959bae2e4dbba6a224f5c2f1
Patricio de la Guardia <padlgt01@gmail.com>
2018-12-19 13:21:57 -0500

    Add react browser router

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import MainContent from './MainContent';
+import About from './About';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 let App = () => {
 	return (
-		<div id="main-div"> 
-			<Header />
-			<MainContent />
-			<Footer />
-		</div>
+		<Router>
+			<div id="main-div"> 
+				<Header />
+				<Route exact path="/" component={ MainContent }></Route>
+				<Route path="/about" component={ About }></Route>
+				<Footer />
+			</div>
+		</Router>
 	)
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<navbar className="nav">
 			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Contact</li>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+				<Link to="/contact">Contact</Link>
 			</ul>
 		</navbar>
 	)
