09ac5873810b8669ed35bcb7212dea94c17baa34
Patricio de la Guardia <padlgt01@gmail.com>
2018-12-19 13:39:06 -0500

    Add Contact component

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,21 @@ import Header from './Header';
 import Footer from './Footer';
 import MainContent from './MainContent';
 import About from './About';
+import Contact from './Contact';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 let App = () => {
 	return (
+		//Create Router instance so that we can use client side routing
 		<Router>
 			<div id="main-div"> 
 				<Header />
+				{/* Define Routes inside Router. Routes have a path and component
+					prop that are mandatory. For the root path be sure to use the exact prop
+				*/}
 				<Route exact path="/" component={ MainContent }></Route>
 				<Route path="/about" component={ About }></Route>
+				<Route path="/contact" component={ Contact }></Route>
 				<Footer />
 			</div>
 		</Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ const Header = () => {
 	return (
 		<navbar className="nav">
 			<ul>
+				{/* Link takes a to prop. When clicked, Link will update your current URL
+				to that specified in the to prop. Therefore rendering the component bound
+				to that specific path */}
 				<Link to="/">Home</Link>
 				<Link to="/about">About</Link>
 				<Link to="/contact">Contact</Link>
